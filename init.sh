435ba8f1f1bf93a19c2214ebf6a19a6351872f40
Nozomi Oshima <oshima@macgraphic.co.jp>
2015-03-24 16:12:27 +0900

    add oh-my-zsh installation

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
+# Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
