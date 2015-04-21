2004e52db64c3d02d419ad6ac2ea9fd83edd1994
mgaoshima <oshima@macgraphic.co.jp>
2015-04-21 11:51:00 +0900

    update

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 
 # dotfiles
-git clone git@github.com:mgaoshima/dotfiles.git ~/dotfiles
+git clone git@github.com:mgaoshima/dotfiles.git ~/.dotfiles
 
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -62,10 +62,10 @@ curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.s
 # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/$(pwd)/.zshrc ~/.zshrc
+ln -sf ~/$(pwd)/.vimrc ~/.vimrc
+ln -sf ~/$(pwd)/.gitconfig ~/.gitconfig
+ln -sf ~/$(pwd)/.gitignore_global ~/.gitignore_global
 
 
 # :NeoBundleInstall を実行 http://qiita.com/yoan/items/6216646324f68e54809d
