96c485d50e6a50e14557004fd5864a62114703e3
Nozomi Oshima <nozomi.oshima@gmail.com>
2015-04-21 12:37:14 +0900

    update

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ cd ~/.dotfiles
 
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew update && brew cask update
 
 brew install ack
 brew install autoconf
@@ -24,9 +26,6 @@ brew install nkf
 brew install node
 brew install python
 brew install tree
-
-brew install caskroom/cask/brew-cask
-
 brew cask install adobe-creative-cloud
 brew cask install appcleaner
 brew cask install atom
@@ -38,22 +37,17 @@ brew cask install google-japanese-ime
 brew cask install imageoptim
 brew cask install integrity
 brew cask install kaleidoscope
-brew cask install keka
-brew cask install macvim
+#brew cask install keka
 brew cask install macwinzipper
-brew cask install mamp
-brew cask install miro-video-converter
-brew cask install mou
+#brew cask install mamp
+#brew cask install miro-video-converter
+#brew cask install mou
 brew cask install name-mangler
 brew cask install qlmarkdown
 brew cask install the-unarchiver
 brew cask install transmit
 brew cask install versions
 brew cask install virtualbox
-
-brew cask cleanup
-
-open /opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
 
 
 # npm
@@ -62,19 +56,21 @@ npm install -g divshot-cli
 npm install -g imageoptim-cli
 
 
-# NeoBundle
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-
-
 # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
+
+# link dotfiles
 ln -sf $(pwd)/.zshrc ~/.zshrc
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
 
 
-# :NeoBundleInstall を実行 http://qiita.com/yoan/items/6216646324f68e54809d
+# NeoBundle - http://qiita.com/yoan/items/6216646324f68e54809d
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 vim +":NeoBundleInstall" +:q
+
+# CreativeCloudをインストール
+open /opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
 
