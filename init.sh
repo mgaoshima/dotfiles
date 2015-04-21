957e8ecc07757828c63f157eccee4b74f2b63d6e
mgaoshima <oshima@macgraphic.co.jp>
2015-04-21 11:48:30 +0900

    update

--- init.sh
+++ init.sh
@@ -6,49 +6,58 @@ git clone git@github.com:mgaoshima/dotfiles.git ~/dotfiles
 
 # Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install vim --with-lua
-brew install macvim --with-cscope --with-lua --HEAD
-brew install\
-  ack\
-  git\
-  go\
-  heroku-toolbelt\
-  hub\
-  lv\
-  nkf\
-  node\
-  tree\
+
+brew install ack
+brew install autoconf
+brew install boot2docker
+brew install brew-cask
+brew install git
+brew install go
+brew install heroku-toolbelt
+brew install hub
+brew install libpng
+brew install lv
+brew install macvim --override-system-vim --with-cscope --with-lua --HEAD
+brew install mercurial
+brew install nkf
+brew install node
+brew install python
+brew install tree
 
 brew install caskroom/cask/brew-cask
-brew cask install\
-  adobe-creative-cloud\
-  appcleaner\
-  coteditor\
-  firefox\
-  google-chrome\
-  google-drive\
-  google-japanese-ime\
-  imageoptim\
-  integrity\
-  kaleidoscope\
-  keka\
-  macwinzipper\
-  mamp\
-  miro-video-converter\
-  mou\
-  name-mangler\
-  qlmarkdown\
-  the-unarchiver\
-  transmit\
-  versions\
-  virtualbox\
+
+brew cask install adobe-creative-cloud
+brew cask install appcleaner
+brew cask install atom
+brew cask install coteditor
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-japanese-ime
+brew cask install imageoptim
+brew cask install integrity
+brew cask install kaleidoscope
+brew cask install keka
+brew cask install macvim
+brew cask install macwinzipper
+brew cask install mamp
+brew cask install miro-video-converter
+brew cask install mou
+brew cask install name-mangler
+brew cask install qlmarkdown
+brew cask install the-unarchiver
+brew cask install transmit
+brew cask install versions
+brew cask install virtualbox
 
 brew cask cleanup
 
 open /opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
 
+
 # NeoBundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
 
 # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -57,6 +66,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
 
 # :NeoBundleInstall を実行 http://qiita.com/yoan/items/6216646324f68e54809d
 vim +":NeoBundleInstall" +:q
