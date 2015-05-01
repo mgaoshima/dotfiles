04189e3ea9ebfc78109e41ae0e316670211f0a8e
mgaoshima <nozomi.oshima@gmail.com>
2015-05-01 16:15:19 +0900

    update

--- init.sh
+++ init.sh
@@ -11,31 +11,31 @@ brew install caskroom/cask/brew-cask
 brew update && brew cask update
 
 brew install ack
-brew install autoconf
+#brew install autoconf
 brew install boot2docker
 brew install brew-cask
 brew install git
 brew install go
 brew install heroku-toolbelt
 brew install hub
-brew install libpng
+#brew install libpng
 brew install lv
 brew install macvim --override-system-vim --with-cscope --with-lua --custom-icons && brew linkapps macvim
 brew install mercurial
 brew install nkf
 brew install node
-brew install python
+#brew install python
 brew install tree
 brew cask install adobe-creative-cloud
 brew cask install appcleaner
-brew cask install atom
+#brew cask install atom
 brew cask install coteditor
-brew cask install firefox
+#brew cask install firefox
 brew cask install google-chrome
 brew cask install google-drive
 brew cask install google-japanese-ime
 brew cask install imageoptim
-brew cask install integrity
+#brew cask install integrity
 brew cask install kaleidoscope
 #brew cask install keka
 brew cask install macwinzipper
@@ -46,7 +46,7 @@ brew cask install name-mangler
 brew cask install qlmarkdown
 brew cask install the-unarchiver
 brew cask install transmit
-brew cask install versions
+#brew cask install versions
 brew cask install virtualbox
 
 
@@ -54,6 +54,7 @@ brew cask install virtualbox
 npm install -g browser-sync
 npm install -g divshot-cli
 npm install -g imageoptim-cli
+npm install -g grunt-cli
 
 
 # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
