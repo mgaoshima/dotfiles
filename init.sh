01b6487275007947926757ce2734a9b14607540e
Nozomi Oshima <nozomi.oshima@gmail.com>
2015-05-22 10:51:19 +0900

    update

--- init.sh
+++ init.sh
@@ -1,74 +1,134 @@
-#!/bin/sh
+#!/bin/bash
+
+
+function brew_install () {
+  if [ `brew list | grep -x $1` ]; then
+    echo "Formula already insatlled: ${1}";
+  else
+    echo "Brewing ${*}..."
+    brew install $*
+  fi
+}
+
+function brew_cask_install () {
+  if [ `brew cask list | grep $1` ]; then
+    echo "Formula already insatlled: ${1}";
+  else
+    echo "Brewing ${*}..."
+    brew cask install $*
+  fi
+}
+
+function brew_tap () {
+  if [ `brew tap | grep -x $1` ]; then
+    echo "Already tapped: ${1}";
+  else
+    brew tap $*
+  fi
+}
+
+function npm_install () {
+  if [ `npm list --depth=0 -g --parseable | grep "${1}$"` ]; then
+    echo "Package already installed: ${1}";
+  else
+    npm install $*
+  fi
+}
+
+function gem_install () {
+  if [ `gem list --local --no-versions | grep -x $1` ]; then
+    echo "Gem already installed: ${1}";
+  else
+    sudo gem install $*
+  fi
+}
 
 
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
-brew update && brew cask update
+if [ `which brew` ]; then
+  echo "Homebrew already installed."
+else
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Installing Homebrew Cask..."
+  brew install caskroom/cask/brew-cask
+fi
 
-brew install ack
-#brew install autoconf
-#brew install boot2docker
-brew install brew-cask
-brew install git
-brew install go
-brew install heroku-toolbelt
-brew install hub
-#brew install libpng
-brew install lv
-brew install macvim --override-system-vim --with-cscope --with-lua --custom-icons && brew linkapps macvim
-brew install mercurial
-brew install nkf
-brew install node
-#brew install python
-brew install tig
-brew install tree
-#brew cask install adobe-creative-cloud
-brew cask install appcleaner
-#brew cask install atom
-brew cask install coteditor
-#brew cask install firefox
-#brew cask install google-chrome
-brew cask install google-drive
-brew cask install google-japanese-ime
-brew cask install imageoptim
-#brew cask install integrity
-brew cask install kaleidoscope
-#brew cask install keka
-brew cask install macwinzipper
-#brew cask install mamp
-#brew cask install miro-video-converter
-#brew cask install mou
-brew cask install name-mangler
-brew cask install qlmarkdown
-brew cask install the-unarchiver
-brew cask install transmit
-#brew cask install versions
-brew cask install virtualbox
+brew_install ack
+brew_install brew-cask
+brew_install git
+brew_install git-lfs
+brew_install go
+brew_install heroku-toolbelt
+brew_install hub
+brew_install hugo
+brew_install libpng
+brew_install lv
+brew_install macvim --override-system-vim --with-cscope --with-lua --custom-icons && brew linkapps macvim
+brew_install nkf
+brew_install node
+brew_install peco
+brew_install tree
+brew_tap motemen/ghq && brew_install ghq
 
+# Cask ~
+brew_cask_install appcleaner
+brew_cask_install coteditor
+brew_cask_install google-chrome
+brew_cask_install google-drive
+brew_cask_install imageoptim
+brew_cask_install integrity
+brew_cask_install kaleidoscope
+brew_cask_install keka
+brew_cask_install macwinzipper
+brew_cask_install name-mangler
+brew_cask_install qlmarkdown
+brew_cask_install the-unarchiver
+brew_cask_install transmit
+
+# Cask /
+brew_cask_install google-japanese-ime
+brew_cask_install virtualbox
 
 # npm
-#npm install -g browser-sync
-#npm install -g divshot-cli
-#npm install -g imageoptim-cli
-#npm install -g grunt-cli
+if [ `which npm` ]; then
+  npm_install browser-sync -g
+  npm_install divshot-cli -g
+  npm_install imageoptim-cli -g
+  npm_install grunt-cli -g
+fi
+
+# Ruby Gems
+if [ `which gem` ]; then
+  gem_install foreman
+  gem_install git-up
+  gem_install jekyll
+fi
+
 
 
 # Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+if [ -e ~/.oh-my-zsh ]; then
+  echo "oh-my-zsh already installed."
+else
+  curl -L http://install.ohmyz.sh | sh
+fi
 
 
 # link dotfiles
+echo "Linking dotfiles..."
 ln -sf $(pwd)/.zshrc ~/.zshrc
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
 
 
+
 # NeoBundle - http://qiita.com/yoan/items/6216646324f68e54809d
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-vim +":NeoBundleInstall" +:q
-
-# CreativeCloudをインストール
-#open /opt/homebrew-cask/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
-
+if [ -e ~/.vim/bundle/neobundle.vim ]; then
+  echo "NeoBundle already installed."
+else
+  echo "Installing NeoBundle..."
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+  vim +":NeoBundleInstall" +:q
+fi
