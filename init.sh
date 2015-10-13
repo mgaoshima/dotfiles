266edb6ab33aee0445847d43cb8bf33518a59763
Nozomi Oshima <nozomi.oshima@gmail.com>
2015-10-13 20:17:08 +0900

    update

--- init.sh
+++ init.sh
@@ -113,7 +113,7 @@ if [ `which npm` ]; then
   npm_install divshot-cli -g
   npm_install imageoptim-cli -g
   npm_install grunt-cli -g
-  npm_install -g phplint jshint csslint htmllint
+  npm_install phplint jshint csslint htmllint -g
 fi
 
 # Ruby Gems
