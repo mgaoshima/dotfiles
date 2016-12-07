752d4990ebf9baaf9f0151efd9db81a567e08e29
Nozomi Oshima <nozomi.oshima@gmail.com>
2016-12-07 19:02:45 +0900

    update

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ alias ga="git add -A; gs"
 alias tarc="tar zcvf"
 alias tarx="tar zxvf"
 alias rsync="rsync --exclude-from=$HOME/.rsync_excludes"
+alias diffcomp="git diff --exit-code --cached --name-only --diff-filter=ACM -- '*.png' '*.jpg' | imageoptim"
 
 # Golang
 set -x GOPATH $HOME/Workspace
@@ -11,7 +12,10 @@ set -x PATH $PATH $GOPATH/bin
 
 # ghq + fzf ... http://qiita.com/unlovingly/items/99999271df7eea7bc953
 function fzf_select_repository
-  ghq list -p | fzf | read line; builtin cd $line
+  ghq list -p | fzf | read repo
+  if test $repo
+    builtin cd $repo
+  end
   commandline -f repaint
 end
 
@@ -19,4 +23,5 @@ end
 function fish_user_key_bindings
   bind \c] fzf_select_repository
   # etc...
+
 end
