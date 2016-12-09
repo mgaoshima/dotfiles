79266773834b1766c6c69651a003c39a692d4e55
Nozomi Oshima <nozomi.oshima@gmail.com>
2016-12-09 15:27:50 +0900

    git

--- config.fish
+++ config.fish
@@ -25,3 +25,20 @@ function fish_user_key_bindings
   # etc...
 
 end
+
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+function fish_prompt
+  set last_status $status
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+  printf '%s ' (__fish_git_prompt)
+  set_color normal
+end
